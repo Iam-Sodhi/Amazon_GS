12ada62a1fbbdf8428048bfaff10ea34e5dd758b
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-08-13 01:26:44 +0530

    changin backend index.js

--- stripe-payment-backend/index.js
+++ stripe-payment-backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const stripe = require('stripe')('sk_test_51NdCXmSFEEyTQkqeoUppjTeUN5fSKfLg5716KkRvPeqMg5xPnTmBrG8anpcXKgPLEioOEjMiOTcdNbUYiqGHB56E00ud23GG2m'); // Replace with your Stripe secret key
 
 const app = express();
-const port = process.env.PORT || 3009;
 
+const stripe = require('stripe')('sk_test_51NdCXmSFEEyTQkqeoUppjTeUN5fSKfLg5716KkRvPeqMg5xPnTmBrG8anpcXKgPLEioOEjMiOTcdNbUYiqGHB56E00ud23GG2m'); // Replace with your Stripe secret key
+const port = process.env.PORT || 3009;
 app.use(bodyParser.json());
+
+app.use(express.static('../public'));
+// let the react app to handle any unknown routes 
+// serve up the index.html if express does'nt recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 
 // Define your API routes
 app.post('/create-payment-intent', async (req, res) => {
