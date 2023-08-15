687ba875b354e0b9eb7e2b245f74ad0f0844a36a
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-08-15 23:07:52 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='home'>
       <div className="home-container">
-        <img className='home-image' src={img1} alt="Banner" />
+        <img className='home-image' src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="Banner" />
 
         <div className="home-row">
         <Product 
